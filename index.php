43c6b0b686e47d638d74bcdf0a9fcd1280b7e64f
bars93 <dan93irbis@mail.ru>
2014-01-11 05:31:29 +0700

    Change version of bootstrap on 2.3.2 adding images adding own css adding bootstrap-responsive support try (to hard) example index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Aquarius</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/common.css">
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="container">
+    <div class="col-md-12 col-md-offset-0 maincontainer">
+        <div class="row">
+            <div class="col-lg-5 pull-right login-form-container">
+                <form class="form-inline login-form" name="logform" method="POST">
+                    <div class="row">
+                        <input type="text" class="input-small" placeholder="Email">
+                        <input type="password" class="input-small" placeholder="Пароль">
+
+
+                    <input type="submit" class="btn btn-primary logbtn" tabindex="8" name="logbtn" value="Вход" >
+                    </div>
+                    <div class="helpreg"><a href="registration.php">Регистрация</a>
+                        </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5 col-md-offset-3">
+            <a href="/"><img src="img/logo.gif" id="logo"></a>
+            </div>
+        </div>
+    </div>
+</body>
+<script type="text/javascript" src="js/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</html>
